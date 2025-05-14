fe96cc18160bd0a177bc38ee4fe40b2866d539ba
pkay97 <007kumarpradeep008@gmail.com>
2025-05-14 15:04:26 +0530

    second commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,10 @@
 "use client"
 
 export default function About(){
-
-    function saveLocal(){
-        localStorage.setItem("name", "Amit")
-    }
     
     return(
         <div>
-            <button className="border border-indigo-400 ml-5" onClick={saveLocal}>Save</button>
+            <button className="border border-indigo-400 ml-5">Save</button>
         </div>
     )
 }
